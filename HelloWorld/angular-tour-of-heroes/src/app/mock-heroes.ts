a724444f32f509088262ed198ae22b773d6413dd
wangyu19810820 <gadfly0036_cn@sina.com>
2018-08-03 14:39:13 +0800

    官方demo

---
+++ HelloWorld/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+  { id: 11, name: 'Mr NICE' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+];
