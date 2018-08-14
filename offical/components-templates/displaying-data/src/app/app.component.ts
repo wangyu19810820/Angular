b8bdd581929022d0386664d87e330f57b740f147
wangyu19810820 <gadfly0036_cn@sina.com>
2018-08-14 10:07:51 +0800

    组件和视图

--- offical/components-templates/displaying-data/src/app/app.component.ts
+++ offical/components-templates/displaying-data/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>My favorite hero is: {{myHero}}</h2>
-  `,
+  // template: `
+  // <h1>{{title}}</h1>
+  // <h2>My favorite hero is: {{myHero}}</h2>
+  // `,
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   myHero = 'Windstorm';
+  hero = new Hero(3, 'cc');
+  heroes: Hero[];
+  isUnchanged = true;
+
+  constructor() {
+    const hero1 = new Hero(1, 'aa');
+    const hero2 = new Hero(2, 'bb');
+    this.heroes = [hero1, hero2];
+  }
 }
