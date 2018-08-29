0ce2f5b466822030eaa5c1f100076ae15a781dd1
wangyu19810820 <gadfly0036_cn@sina.com>
2018-08-29 22:27:48 +0800

    表单

--- offical/form/src/app/app.module.ts
+++ offical/form/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
@@ -10,7 +11,8 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
     HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
